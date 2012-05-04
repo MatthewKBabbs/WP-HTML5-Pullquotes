1c0596f3deb23b5cd0868eed72f6c466e32eae85
Matthew K Babbs <matthew@intangiblestyle.com>
2012-05-04 15:14:51 +0100

    Initial commit of plugin boilerplate

---
+++ intangible-pullquotes/intangible-pullquotes.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Intangible-Pullquotes
+Plugin URI: http://intangiblestyle.com/lab/intangible-pullquotes-wordpress-plugin/
+Description: Create pullquotes in WordPress posts without duplicte content, using HTML5.
+Version: 0.1
+Author: Matthew K Babbs
+Author URI: http://intangiblestyle.com/
+License: GPLv2
+*/
+?>
+<?php
+/*  Copyright 2012  Matthew K Babbs  (email : matthew@intangiblestyle.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
