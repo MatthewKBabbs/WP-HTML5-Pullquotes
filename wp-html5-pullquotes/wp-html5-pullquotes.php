1f02ef677982b12187fac097f2debc31a83ca31b
Matthew K Babbs <matthew@intangiblestyle.com>
2012-05-07 03:16:34 +0100

    Aand... disabling the accidentally-enabled editor CSS again

--- wp-html5-pullquotes/wp-html5-pullquotes.php
+++ wp-html5-pullquotes/wp-html5-pullquotes.php
@@ -39,6 +39,7 @@ function istyle_data_pullquote_attributes($initArray) {
 add_filter('tiny_mce_before_init', 'istyle_data_pullquote_attributes');
 
 // add styles to display in TinyMCE visual editor
+/*
 function istyle_data_pullquote_mce_css($mce_css) {
 	if (!empty($mce_css)) {
 		$mce_css .= ',';
@@ -47,7 +48,7 @@ function istyle_data_pullquote_mce_css($mce_css) {
 	return $mce_css;
 }
 add_filter( 'mce_css', 'istyle_data_pullquote_mce_css' );
-
+*/
 
 // wp_enqueue_style to add css file with default styling for pullquotes
 ?>
