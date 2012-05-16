6415c13d222749665af2bb61a7cdea0e530222a1
Matthew K Babbs <matthew@intangiblestyle.com>
2012-05-16 12:24:55 +0100

    All options now in admin page (fixes #3)

--- src/settings.php
+++ src/settings.php
@@ -15,11 +15,13 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+// Add plugin to the Wordpress Admin menus
 add_action('admin_menu', 'html5_pullquotes_menu');
 
 function html5_pullquotes_menu() {
 	add_theme_page('HTML5 Pullquotes', 'HTML5 Pullquotes', 'manage_options', 'html5-pullquotes', 'html5_pullquotes_settings_page');
 }
+
 // html5_pullquotes_options_page() displays the content for the plugin's settings page
 function html5_pullquotes_settings_page() {
 	if (!current_user_can('manage_options')) {
@@ -41,40 +43,104 @@ function html5_pullquotes_settings_page() {
 add_action('admin_init', 'html5_pullquotes_admin_init');
 function html5_pullquotes_admin_init(){
 	register_setting( 'html5_pullquotes_plugin_options', 'html5_pullquotes_plugin_options', 'html5_pullquotes_plugin_options_validate' );
+
 	add_settings_section('plugin_styles', 'Pullquote Styles', 'plugin_styles_section_callback', 'html5_pullquotes_plugin');
 	add_settings_section('legacy_compatibility', 'Legacy Compatibility', 'legacy_compatibility_section_callback', 'html5_pullquotes_plugin');
 
 	add_settings_field('inject_pullquote_styles', 'Inject pullquote styles into my theme', 'inject_pullquote_styles_callback', 'html5_pullquotes_plugin', 'plugin_styles');
-	add_settings_field('pullquote_font_size','Pullquote font-size','pullquote_font_size_callback','html5_pullquotes_plugin','plugin_styles');
+	add_settings_field('pullquote_font_size', 'Pullquote font-size', 'pullquote_font_size_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('show_horiz_borders', 'Show top and bottom borders', 'show_horiz_borders_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('show_vert_borders', 'Show left and right borders', 'show_vert_borders_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('border_style', 'Pullquote border-style', 'border_style_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('border_width', 'Pullquote border-width', 'border_width_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('border_color', 'Pullquote border-color', 'border_color_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('background_color', 'Pullquote background-color', 'background_color_callback', 'html5_pullquotes_plugin', 'plugin_styles');
+	add_settings_field('force_legacy_compatibility', 'Enable compatibility with legacy browsers', 'force_legacy_compatibility_callback', 'html5_pullquotes_plugin', 'legacy_compatibility');
 }
 
 // Callback to provide descriptive text for plugin_styles section
 function plugin_styles_section_callback() {
-	echo '<p>Main description of this section here.</p>';
+	echo '<p>Choose how pullquotes should be styled. Don&rsquo;t disable style injection unless your theme already provides styles for HTML5 pullquotes.</p>';
 }
 // Callback to provide descriptive text for legacy_compatibility section
 function legacy_compatibility_section_callback() {
-	echo '<p>Enable compatibility with legacy browsers IE6 and IE7. It&rsquo;s recommended to check your site logs before enabling this, as many sites have very few visits from such old browsers!</p>';
+	echo '<p>Enable pullquote display in legacy browsers IE6 and IE7. It&rsquo;s recommended to check your site logs before enabling this, as many sites have very few visits from such old browsers! This setting will add extra JavaScript files to your theme, so visitors may also see the site taking a few seconds longer to load.</p>';
 }
 
-// Callback to display input for inject_pullquote_styles settings field
+// Callbacks to display input for settings fields
 function inject_pullquote_styles_callback() {
 					$options = get_option('html5_pullquotes_plugin_options');
 					echo "<input name='html5_pullquotes_plugin_options[inject_pullquote_styles]' type='checkbox' value='1' ";
 					echo (checked($options['inject_pullquote_styles'],true,false));
 					echo " />";
-					if (!isset($options['inject_pullquote_styles'])) {
-						echo "Option not found!";
-					} else {
-						echo 'Option found; value is: \''.$options['inject_pullquote_styles'].'\'.';
-					}
 }
-// Callback to display input for plugin_font_size settings field
 function pullquote_font_size_callback() {
 	$options = get_option('html5_pullquotes_plugin_options');
 	echo "<input name='html5_pullquotes_plugin_options[pullquote_font_size]' size='25' type='text' value='";
 	echo $options['pullquote_font_size'];
 	echo "' />";
+}
+function show_horiz_borders_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	echo "<input name='html5_pullquotes_plugin_options[show_horiz_borders]' type='checkbox' value='1' ";
+	echo (checked($options['show_horiz_borders'],true,false));
+	echo " />";
+}
+function show_vert_borders_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	echo "<input name='html5_pullquotes_plugin_options[show_vert_borders]' type='checkbox' value='1' ";
+	echo (checked($options['show_vert_borders'],true,false));
+	echo " />";
+}
+function border_style_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	$possible_values = array(
+		'1' => 'none',
+		'2' => 'dotted',
+		'3' => 'dashed',
+		'4' => 'solid',
+		'5' => 'double',
+		'6' => 'groove',
+		'7' => 'ridge',
+		'8' => 'inset',
+		'9' => 'outset'
+	);
+	if (!isset($options['border_style'])) {
+		$options['border_style'] = 'solid';
+	}
+	echo "<select name='html5_pullquotes_plugin_options[border_style]' >";
+	foreach ($possible_values as $i) {
+		echo "<option value=\"".$i."\"";
+		if ($i == $options['border_style']) {
+			echo " selected=\"selected\" ";
+		}
+		echo ">".$i."</option>";
+	}
+	echo "</select>";
+}
+function border_width_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	echo "<input name='html5_pullquotes_plugin_options[border_width]' size='25' type='text' value='";
+	echo $options['border_width'];
+	echo "' />";
+}
+function border_color_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	echo "<input name='html5_pullquotes_plugin_options[border_color]' size='25' type='text' value='";
+	echo $options['border_color'];
+	echo "' />";
+}
+function background_color_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	echo "<input name='html5_pullquotes_plugin_options[background_color]' size='25' type='text' value='";
+	echo $options['background_color'];
+	echo "' />";
+}
+function force_legacy_compatibility_callback() {
+	$options = get_option('html5_pullquotes_plugin_options');
+	echo "<input name='html5_pullquotes_plugin_options[force_legacy_compatibility]' type='checkbox' value='1' ";
+	echo (checked($options['force_legacy_compatibility'],true,false));
+	echo " />";
 }
 
 // Validate plugin options
@@ -87,22 +153,8 @@ function html5_pullquotes_plugin_options_validate($input) {
 	// 2: validate checkbox setting
 	if (isset($input['inject_pullquote_styles']) && ($input['inject_pullquote_styles'] == true)) {
 		$valid_input['inject_pullquote_styles'] = true;
-	}
+	} else $valid_input['inject_pullquote_styles'] = false;
 	// Last: return array with all valid inputs
 	return $valid_input;
 }
-/* Options:
-		* Inject pullquote styles into my theme
-			* Font-family [Should this be implied?]
-			* font-size [imply line-height based on font-size]
-			* borders	[default margins & padding to sensible values]
-			* background-colour
-		* Force compatibility with old versions of Internet Explorer
-		
-	Process:
-		* on_activate -> set defaults
-			register_activation_hook( __FILE__, 'myplugin_activate' );
-		* when saving -> set new values
-		* on deactivate -> delete values?
-*/
 ?>
