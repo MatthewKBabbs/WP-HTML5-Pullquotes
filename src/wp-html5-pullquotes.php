d1b0a7eda6a04ce8f6b389e0aacfb27b1322af2f
Matthew K Babbs <matthew@intangiblestyle.com>
2012-06-12 00:01:38 +0100

    First pass at injecting styles based on pref (refs #2, refs #4, refs #6)

--- src/wp-html5-pullquotes.php
+++ src/wp-html5-pullquotes.php
@@ -45,15 +45,38 @@ function istyle_data_pullquote_attributes($initArray) {
 }
 add_filter('tiny_mce_before_init', 'istyle_data_pullquote_attributes');
 
-// add styles to display in TinyMCE visual editor
+// Define function to pull it all together, and add it to the wp_enqueue_scripts hook
+function html5_pullquotes() {
+	// Collect options
+	$html5_pullquotes_options = get_option('html5_pullquotes_plugin_options');
+
+	// Register styles & script for theme
+	wp_register_style( 'html5-pullquote-styles', plugins_url('pullquote-styles.php?theme.css', __FILE__) );
+	wp_register_script('html5-pullquotes-legacy-compatibility', plugins_url('html5-pullquote-legacy-support.js', __FILE__), array('jquery'), false, true);
+
+	// If injecting styles:
+	// 1: add styles to display in TinyMCE editor
+	// 2: enqueue style to display in theme
+	if($html5_pullquotes_options['inject_pullquote_styles'] == true) {
+		add_filter( 'mce_css', 'istyle_data_pullquote_mce_css' );
+		wp_enqueue_style( 'html5-pullquote-styles' );
+	}
+
+	// If forcing legacy compatiblity, enqueue script
+	if($html5_pullquotes_options['force_legacy_compatibility'] == true) {
+		wp_enqueue_script('html5-pullquotes-legacy-compatibility');
+	}
+}
+add_action( 'wp_enqueue_scripts', 'html5_pullquotes');
+
+
+// Function to add styles to display in TinyMCE visual editor
 function istyle_data_pullquote_mce_css($mce_css) {
 	if (!empty($mce_css)) {
 		$mce_css .= ',';
 	}
-	$mce_css .= plugins_url('wp-html5-pullquote-editor.css', __FILE__);
+	$mce_css .= plugins_url('pullquote-styles.php?editor.css', __FILE__);
 	return $mce_css;
 }
-add_filter( 'mce_css', 'istyle_data_pullquote_mce_css' );
 
-// wp_enqueue_style to add css file with default styling for pullquotes
 ?>
